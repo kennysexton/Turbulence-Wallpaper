e8395cb9620bbf9dd228f1ae6b82d4083938ed3b
kennysexton <kennysexton4@gmail.com>
2025-12-12 09:59:48 -0700

    bottom icons at smaller sizes

--- src/renderer/Preview.jsx
+++ src/renderer/Preview.jsx
@@ -6,7 +6,7 @@ function Preview({apiKey, searchTerms, currentPhoto}) {
 	const unsplashReferralLink = `https://unsplash.com/?utm_source=${appName}&utm_medium=referral`;
 
 	return (
-		<div className="bg-gray-800 w-full h-full">
+		<div className=" text-slate-800 w-full h-full">
 			{currentPhoto ? (
 					<div className="w-full h-full">
 						<div className="w-full h-full">
@@ -16,26 +16,28 @@ function Preview({apiKey, searchTerms, currentPhoto}) {
 								className="absolute inset-0 w-full h-full object-cover object-center"
 							/>
 						</div>
-						<div className="absolute bottom-2 bg-gray-300 w-full flex justify-between">
-							<p className="text-gray-700"><strong>By:</strong>{' '}
+						<div className="absolute bottom-0 backdrop-blur-md bg-white/40 py-2 px-4 w-full flex flex-col sm:flex-row justify-between ">
+							<p className=""><strong>By:</strong>{' '}
 								{currentPhoto.userName ? (
 									<a href={currentPhoto.userProfileUrl} target="_blank" rel="noopener noreferrer"
-										 className="text-blue-500 hover:underline">
+										 className="hover:underline">
 										{currentPhoto.userName}
 									</a>
-								) : 'N/A'}
+								) : ''}
 								{' '} on{' '}
 								<a href={unsplashReferralLink} target="_blank" rel="noopener noreferrer"
-									 className="text-blue-500 hover:underline">
+									 className="hover:underline">
 									Unsplash
 								</a>
 							</p>
-							<p className="text-gray-700"><strong>Location:</strong> {currentPhoto.locationName || 'N/A'}</p>
+							{currentPhoto && currentPhoto.locationName && (
+								<p className=""><strong>Location:</strong> {currentPhoto.locationName}</p>
+							)}
 						</div>
 					</div>
 				) :
 				(
-					<p className="text-gray-700 text-center mb-4">No wallpaper information available. Please set your
+					<p className="text-center mb-4">No wallpaper information available. Please set your
 						API key in the
 						settings.</p>
 				)
